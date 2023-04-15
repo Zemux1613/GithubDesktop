690ec88717ecac8236e90161170dc0905ba38b1f
Tom L <devzemux1613@gmail.com>
2023-04-15 15:48:35 +0200

    feat: init

---
+++ index.php
@@ -0,0 +1,108 @@
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="assets/globalStyle.css">
+</head>
+<body>
+
+<div id="userArea">
+    <h3>Benutzerdaten</h3>
+    <img id="profilImage" alt="no image found">
+    <p id="username"></p>
+    <p id="bio"></p>
+    <p id="creation"></p>
+</div>
+
+<h3><?php if (!isset($_GET['repo'])) echo "Alle Projekte (<span id='repoCount'></span>)"; ?></h3>
+<ul id="repos">
+
+</ul>
+<?php
+
+if (isset($_GET['repo'])) {
+    $repoName = $_GET['repo'];
+    $requestUrl = "https://api.github.com/repos/Zemux1613/" . $repoName;
+    echo "<script>
+        let userArea = document.getElementById('userArea');
+        userArea.style.display = 'none';
+        
+        let request = new XMLHttpRequest();
+        request.open('GET', '" . $requestUrl . "');
+        request.send();
+        request.onreadystatechange = () => {
+            if (request.readyState === 4 && request.status === 200) {
+                let doc = document.getElementById('repos');
+                let item = JSON.parse(request.response);
+                let li1 = document.createElement('li');
+                let li2 = document.createElement('li');
+                let li3 = document.createElement('li');
+                let li4 = document.createElement('li');
+                let li5 = document.createElement('li');
+                li1.innerText = 'URL: ' + item.url;
+                li2.innerText = 'Description: ' + item.description;
+                li3.innerText = 'Erstellt am ' + item.created_at;
+                li4.innerText = 'Letzte Aktivität am ' + item.pushed_at;
+                li5.innerText = 'Erstellt am ' + item.created_at;
+                doc.appendChild(li1);
+                doc.appendChild(li2);
+                doc.appendChild(li3);
+                doc.appendChild(li4);
+                doc.appendChild(li5);
+            }
+        }
+        
+        let branches = new XMLHttpRequest();
+        branches.open('GET', 'https://api.github.com/repos/Zemux1613/".$repoName."/branches');
+        branches.send();
+        branches.onreadystatechange = () => {
+            if (branches.readyState === 4 && branches.status === 200) {
+                let doc = document.getElementById('repos');
+                let branchCount = 0;
+                let item = JSON.parse(branches.response).forEach(branches => {
+                    branchCount++;
+                })
+                console.log(item)
+                let li1 = document.createElement('li');            
+                li1.innerText = 'Branches: ' + branchCount;
+                doc.appendChild(li1);         
+            }
+        }
+    </script>";
+} else {
+    echo "<script>
+        function requestUrl(url) {
+            let request = new XMLHttpRequest();
+            request.open('GET', url);
+            request.send();
+            return request;
+        }
+        let request = requestUrl('https://api.github.com/users/Zemux1613/repos?per_page=100');
+        request.onreadystatechange = () => {
+                if (request.readyState === 4 && request.status === 200) {
+                    let doc = document.getElementById('repos');
+                    JSON.parse(request.response).forEach(item => {
+                        let li = document.createElement('li');
+                        let node = document.createElement('a');
+                        node.href = 'index.php?repo=' + item.name;
+                        node.innerText = item.name + (item.archived === true ? ' (Archiviert)' : '');
+                        li.appendChild(node)
+                        doc.appendChild(li)
+                    })
+                }
+            }
+            
+        let userRequest = requestUrl('https://api.github.com/users/Zemux1613');
+        userRequest.onreadystatechange = () => {
+                if (userRequest.readyState === 4 && userRequest.status === 200) {
+                    let item  = JSON.parse(userRequest.response);
+                    document.getElementById('username').innerText = item.login + (item.name != null ? ' - ' +  item.name : '');
+                    document.getElementById('profilImage').src = item.avatar_url;
+                    document.getElementById('creation').innerText = 'Erstellt am ' + item.created_at;
+                    document.getElementById('repoCount').innerText = item.public_repos;
+                    document.getElementById('bio').innerText = item.bio;
+                }
+            }
+    </script>";
+}
+?>
+</body>
+</html>
