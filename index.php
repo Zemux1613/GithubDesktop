4ee326ee2512c8f7ae03b8cf3182397f81107f23
Tom L. <devzemux1613@gmail.com>
2024-03-07 19:08:59 +0100

    design: fix footer position

--- index.php
+++ index.php
@@ -2,26 +2,26 @@
 session_start();
 $view = "loginPage";
 
-if(isset($_GET['view'])) {
+if (isset($_GET['view'])) {
     $view = $_GET['view'];
 } ?>
-    <!DOCTYPE html>
-    <html lang="de">
-    <head>
-        <title>Github Desktop</title>
-        <meta charset="UTF-8">
-        <link href="assets/global.css" rel="stylesheet">
-        <?php if (file_exists("assets/$view.css")) {
-            echo '<link rel="stylesheet" href="assets/' . $view . '.css?' . time() . '">';
-        } ?>
-        <link href="https://github.githubassets.com/assets/pinned-octocat-093da3e6fa40.svg" rel="icon">
-    </head>
-    <body>
-    <noscript>You require JavaScript.</noscript>
-    <?php
-        require_once ("views/$view.php");
-        require_once ("views/footer.php");
-    ?>
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <title>Github Desktop</title>
+    <meta charset="UTF-8">
+    <link href="assets/global.css" rel="stylesheet">
+    <?php if (file_exists("assets/$view.css")) {
+        echo '<link rel="stylesheet" href="assets/' . $view . '.css?' . time() . '">';
+    } ?>
+    <link href="https://github.githubassets.com/assets/pinned-octocat-093da3e6fa40.svg" rel="icon">
+</head>
+<body>
+<noscript>You require JavaScript.</noscript>
+<?php
+require_once("views/$view.php");
+require_once("views/footer.php");
+?>
 </body>
 </html>
 
